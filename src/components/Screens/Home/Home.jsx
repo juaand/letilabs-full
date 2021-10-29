7d4b91dcb72078c731ac230f972922ddf42a463b
Andres Martinez <andresmartgonz@gmail.com>
2021-10-29 20:45:48 +0200

    ajustes de código

--- src/components/Screens/Home/Home.jsx
+++ src/components/Screens/Home/Home.jsx
@@ -1,6 +1,5 @@
 import './Home.css'
 import React from 'react'
-import Header from '../../Header/Header'
 import Video from './Video/Video'
 import UsInfo from './UsInfo/UsInfo'
 import Carousel from './Carousel/Carousel'
