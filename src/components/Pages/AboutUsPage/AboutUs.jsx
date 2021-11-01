49fe7539a92078e58bb626152b6565cad33dc600
Andres Martinez <andresmartgonz@gmail.com>
2021-11-01 16:51:40 +0100

    rct009 - rct010

--- src/components/Pages/AboutUsPage/AboutUs.jsx
+++ src/components/Pages/AboutUsPage/AboutUs.jsx
@@ -2,11 +2,13 @@ import './AboutUs.css'
 import React from 'react'
 import Header from '../../Header/Header'
 import Banner from './Banner/Banner'
+import MarcandoPauta from './MarcandoPauta/MarcandoPauta'
 
 function AboutUs() {
     return (
         <main>
             <Banner />
+            <MarcandoPauta/>
         </main>
     )
 }
