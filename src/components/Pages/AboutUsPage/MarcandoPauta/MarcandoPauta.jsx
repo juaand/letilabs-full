0ab0b73bb0cc1cfa768b9c79c7a5e5c81c807c7d
Andres Martinez <andresmartgonz@gmail.com>
2021-11-04 13:53:41 +0100

    css008

--- src/components/Pages/AboutUsPage/MarcandoPauta/MarcandoPauta.jsx
+++ src/components/Pages/AboutUsPage/MarcandoPauta/MarcandoPauta.jsx
@@ -4,15 +4,16 @@ import React from 'react'
 function MarcandoPauta() {
     return (
         <section className="container-fluid MarcandoPauta">
-            <div className="container">
-                <div className="row justify-content-between">
-                    <div className="col-12 col-sm-5 MarcandoPauta__text">
-                        <p>
-                            Contamos con un talento humano calificado y cualificado que con su <span className="blue-text">ingenio</span> e <span className="blue-text">increíble calidad humana,</span> trabajan día a día por los venezolanos que velan por su salud y la de los demás
-                        </p>
+            <div className="row justify-content-between">
+                <div className="col-12 col-sm-5 MarcandoPauta__text">
+                    <div className="row justify-content-center">
+                        <div className="col-11 col-sm-9 offset-sm-5">
+                            <p>
+                                Contamos con un talento humano calificado y cualificado que con su <span className="blue-text">ingenio</span> e <span className="blue-text">increíble calidad humana,</span> trabajan día a día por los venezolanos que velan por su salud y la de los demás
+                            </p></div>
                     </div>
-                    <div className="col-12 col-sm-6 MarcandoPauta__bg"></div>
                 </div>
+                <div className="col-12 col-sm-6 MarcandoPauta__bg"></div>
             </div>
         </section>
     )
