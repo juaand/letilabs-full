0b204948eeef2e893c321ca0c40e2e35336edda3
Andres Martinez <andresmartgonz@gmail.com>
2021-11-06 13:12:34 +0100

    rct025

---
+++ src/components/Pages/AdminFarVigPage/AdminFarVigPage.jsx
@@ -0,0 +1,12 @@
+import './AdminFarVigPage.css'
+import React from 'react'
+
+function AdminFarVigPage() {
+    return (
+        <div>
+            <h1>FARMACO VIGILANCIA PAGE</h1>
+        </div>
+    )
+}
+
+export default AdminFarVigPage
