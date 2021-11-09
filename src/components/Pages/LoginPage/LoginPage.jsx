d6957f7bad0b689f42cc7857d035abe73fab1aef
Andres Martinez <andresmartgonz@gmail.com>
2021-11-09 17:56:04 +0100

    admin login path updated

--- src/components/Pages/LoginPage/LoginPage.jsx
+++ src/components/Pages/LoginPage/LoginPage.jsx
@@ -50,7 +50,7 @@ const LoginPage = () => {
     const isError = Object.values(error).some(err => err)
 
     if (user && user.role === 'Admin') {
-        return <Redirect to="/admin" />
+        return <Redirect to="/admin-farmacovigilancia" />
     }
 
     return (
