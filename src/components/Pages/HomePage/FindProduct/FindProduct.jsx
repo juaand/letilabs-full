aa323d8df6e23b82eab3cdb96284a39c1c83779d
Andres Martinez <andresmartgonz@gmail.com>
2021-11-11 21:20:09 +0100

    rct036

--- src/components/Pages/HomePage/FindProduct/FindProduct.jsx
+++ src/components/Pages/HomePage/FindProduct/FindProduct.jsx
@@ -1,6 +1,5 @@
 import './FindProduct.css'
 import React from 'react'
-import {useHistory} from 'react-router-dom'
 import {useFormState} from '../../../../hooks/useFormState'
 import InputWithLabel from '../../../Form/InputWithLabel/InputWithLabel'
 import DropdownWithLabel from '../../../Form/DropdownWithLabel/DropdownWithLabel'
@@ -28,47 +27,53 @@ function FindProduct() {
     )
 
     const {data, error, touch} = state
-    const history = useHistory()
 
     const searchSubmit = (event) => {
         event.preventDefault()
 
-        history.push({
-            pathname: '/buscar',
-            state: {
-                buscar: data.search,
-                hideSearchIcon: true
-            }
-        })
+        console.log('hola')
+
+        // history.push({
+        //     pathname: '/buscar',
+        //     state: {
+        //         buscar: data.search,
+        //         hideSearchIcon: true
+        //     }
+        // })
     }
     return (
         <section className="container-fluid FindProduct">
             <div className="container">
-                <div className="row justify-content-center">
+                <div className="row FindProduct__row">
                     <div className="col-12 col-sm-10">
                         <h1>Encuentra un producto...</h1>
-                        <form onSubmit={searchSubmit} className="container">
-                            <div className="FindProduct__form input-group">
-                                <InputWithLabel
-                                    value={data.search}
-                                    onBlur={onBlur}
-                                    onChange={onChange}
-                                    name="search"
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="Escribe el nombre o condición"
-                                />
-                                <DropdownWithLabel
-                                    placeholder="Especialidad médica"
-                                    value={data.especialidad}
-                                    label=""
-                                    name="especialidad"
-                                    onChange={onChange}
-                                    className={`form-control ${touch.especialidad && error.especialidad ? "is-invalid" : ""}`}
-                                    list="especialidades"
-                                    data={dataSpecialities}
-                                />
-                                <span class="input-group-text">Buscar</span>
+                        <form className="FindProduct__form">
+                            <div className="input-group">
+                                <div className="col-12 p-0 col-sm-7 FindProduct__label">
+                                    <InputWithLabel
+                                        value={data.search}
+                                        onBlur={onBlur}
+                                        onChange={onChange}
+                                        name="search"
+                                        type="text"
+                                        className="InputWithLabel form-control"
+                                        placeholder="Escribe el nombre o condición"
+                                    />
+                                </div>
+                                <div className="col-12 p-0 col-sm-4">
+                                    <DropdownWithLabel
+                                        placeholder="Especialidad médica"
+                                        value={data.especialidad}
+                                        label=""
+                                        name="especialidad"
+                                        onChange={onChange}
+                                        className={`form-control ${touch.especialidad && error.especialidad ? "is-invalid" : ""}`}
+                                        list="especialidades"
+                                        data={dataSpecialities}
+                                    />
+                                </div>
+                                <div onClick={searchSubmit} className="col-12 p-0 col-sm-1 leti-btn">
+                                </div>
                             </div>
                         </form>
                     </div>
