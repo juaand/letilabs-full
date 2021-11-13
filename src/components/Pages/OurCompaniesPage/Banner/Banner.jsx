9126a88ccaff01c134a90c59d9fe1598460f7799
Andres Martinez <andresmartgonz@gmail.com>
2021-11-13 12:17:02 +0100

    rct040 Banner

--- src/components/Pages/OurCompaniesPage/Banner/Banner.jsx
+++ src/components/Pages/OurCompaniesPage/Banner/Banner.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 
 function Banner() {
     return (
-        <section className="container-fluid Banner">
+        <section className="container-fluid Banner__ourCompanies">
             <div className="container">
                 <div className="row d-flex">
-                    <div className="col-12 col-sm-6 Banner__title">
+                    <div className="col-12 col-sm-6 Banner__ourCompanies__title">
                         <h1>Nos conformamos por tres grandes unidades de negocio, que se dedican a diferentes áreas, pero trabajan entre ellas para lograr <span className="blue-text">mejores resultados</span></h1>
                     </div>
-                    <div className="col-12 col-sm-6 Banner__bg"></div>
+                    <div className="col-12 col-sm-6 Banner__ourCompanies__bg"></div>
                 </div>
             </div>
             <div className="leti-blue-triangle parallax" data-speed="-.2" data-axis="vertical"></div>
