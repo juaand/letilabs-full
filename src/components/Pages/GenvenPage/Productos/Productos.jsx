10a8b476e99c111836033945f8bd9b1c0170cc0b
Andres Martinez <andresmartgonz@gmail.com>
2021-11-15 18:20:24 +0100

    genven resp

--- src/components/Pages/GenvenPage/Productos/Productos.jsx
+++ src/components/Pages/GenvenPage/Productos/Productos.jsx
@@ -9,7 +9,7 @@ function Productos() {
             <div className="Productos__genven-002 parallax" data-axis="horizontal" data-speed="0.17"/>
             <div className="Productos__genven-003 parallax" data-axis="horizontal" data-speed="0.1"/>
             <div className="row">
-                <div className="col-11 col-sm-5 offset-sm-1 Productos__info">
+                <div className="col-9 col-sm-5 offset-sm-1 Productos__info">
                     <h1 className="Productos__desc">Ofrecemos terapias en las principales áreas terapéuticas: Cardiovascular, anti-infecciosos, anti-inflamatorios y analgésicos.</h1>
                     <Link to="/productos" className="leti-btn">Conoce los productos</Link>
                 </div>
