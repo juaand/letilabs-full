84e66c8b62fb3c2553cfbc6d7adba3c7a715ed08
Andres Martinez <andresmartgonz@gmail.com>
2021-11-15 18:54:37 +0100

    bio banner resp

--- src/components/Pages/BiocontrolledPage/InfoCards/InfoCards.jsx
+++ src/components/Pages/BiocontrolledPage/InfoCards/InfoCards.jsx
@@ -1,14 +1,14 @@
 import './InfoCards.css'
 import React from 'react'
-import dataLeti from '../../../../data/dataLeti'
+import dataBiocontrolled from '../../../../data/dataBiocontrolled'
 
 
 function InfoCards() {
     return (
         <section className="container InfoCards">
             <div className="row justify-content-around">
-                {dataLeti.map(el =>
-                    <div className="col InfoCards__cards">
+                {dataBiocontrolled.map(el =>
+                    <div className="col-12 col-sm-4 InfoCards__cards">
                         <h4>{el.title}</h4>
                         <p>{el.info}</p>
                     </div>)}
---
+++ src/data/dataBiocontrolled.js
@@ -0,0 +1,14 @@
+const dataBiocontrolled = [
+  {
+    id: 1,
+    title: 'Expertos en liberación modificada',
+    info: 'Aliquam egestas orci augue, ac elementum nisi ullamcorper et. Integer vestibulum, lacus congue posuere sollicitudin, libero mauris efficitur lacus, a dapibus neque tortor interdum eros. Nam varius lorem ac ante aliquet lacinia.'
+  },
+  {
+    id: 2,
+    title: 'Tecnología para innovar',
+    info: 'Phasellus eget lorem sed magna ornare blandit commodo sed dolor. Duis ornare pulvinar tellus at interdum. '
+  },
+]
+
+export default dataBiocontrolled
