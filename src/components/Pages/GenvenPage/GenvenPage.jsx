cc900954b3a7a81dfb1d6339caa697679e146cea
Andres Martinez <andresmartgonz@gmail.com>
2021-11-15 17:15:01 +0100

    timeline

---
+++ src/components/Pages/GenvenPage/GenvenPage.jsx
@@ -0,0 +1,55 @@
+import './GenvenPage.css'
+import React, {useEffect} from 'react'
+import {createContent} from '../../../services/ApiClient'
+import {useAuthContext} from '../../../contexts/AuthContext'
+import Seo from '../../Seo/Seo'
+import Banner from './Banner/Banner'
+import Video from './Video/Video'
+import Timeline from './Timeline/Timeline'
+import Equipo from './Equipo/Equipo'
+
+
+function GenvenPage() {
+    const {user} = useAuthContext()
+    const data = {
+        content: [],
+        url: '/genven',
+        name: 'Genven'
+    }
+
+    useEffect(() => {
+        if (user) {
+            const mainContent = document.querySelectorAll('p, h1, h2, h3, h4, h5, h6')
+            mainContent.forEach(content => {
+                data.content.push(content.innerText)
+            })
+
+            const fetchData = async () => {
+                await createContent(data)
+            }
+            fetchData()
+        }
+
+        const isMenuOpen = document.querySelector('.show')
+
+        if (isMenuOpen) {
+            isMenuOpen.classList.remove('show')
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return (
+        <>
+            <Seo title='Grupo Leti | Genven' name='description' content="Genven Genéricos Venezolanos, es nuestra línea de genéricos de Laboratorios Leti S.A.V, con más de 25 años en el mercado farmacéutico venezolano." />
+            <main>
+                <Banner />
+                <Video />
+                <Timeline />
+                <Equipo />
+            </main>
+        </>
+    )
+}
+
+export default GenvenPage
---
+++ src/data/genvenTimeline.js
@@ -0,0 +1,14 @@
+const genvenTimeline = [
+  {
+    id: '1',
+    imgURL: 'box.jpg',
+    desc: '<span class="blue-text">1$ Box,</span><br/> una presentación que cabe en el bolsillo de todos'
+  },
+  {
+    id: '2',
+    imgURL: 'box.jpg',
+    desc: '<span class="blue-text">2$ Box,</span><br/> una presentación que cabe en el bolsillo de todos'
+  }
+]
+
+export default genvenTimeline
