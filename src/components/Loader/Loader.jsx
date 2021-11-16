423daf2a5bb2840d507f993d7ccf7fcafd7b6de5
Andres Martinez <andresmartgonz@gmail.com>
2021-11-16 17:54:00 +0100

    loader text

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -7,6 +7,7 @@ function Loader() {
             <div className="Loader__container">
                 <div className="Loader__stroke__red"></div>
                 <div className="Loader__stroke__blue"></div>
+                <h1 className="Loader__text">Cargando</h1>
             </div>
         </section>
     )
