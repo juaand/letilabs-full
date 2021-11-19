53f9a275ee6d068f91111545c0dbabbfd929cec2
Andres Martinez <andresmartgonz@gmail.com>
2021-11-19 20:11:40 +0100

    Edit admin update

--- src/components/Pages/AdminEditPage/Inicio/Inicio.jsx
+++ src/components/Pages/AdminEditPage/Inicio/Inicio.jsx
@@ -1,6 +1,4 @@
-import React, {Suspense} from 'react'
-import Seo from '../../../Seo/Seo'
-import Loader from '../../../Loader/Loader'
+import React from 'react'
 import Video from '../../HomePage/Video/Video'
 import Carousel from '../../HomePage/Carousel/Carousel'
 import Unidades from '../../HomePage/Unidades/Unidades'
@@ -13,8 +11,6 @@ function Inicio() {
 
     return (
         <>
-            <Seo title='Grupo Leti | Inicio' name='description' content="Laboratorios Leti es un laboratorio farmacéutico venezolano que desde hace 70 años, crea soluciones de salud a través de la producción y comercialización de un amplio portafolio de medicamentos desarrollados con tecnología y seguridad, de la mano de un talento humano caliﬁcado que trabaja día a día para acompañar a los venezolanos." />
-            <Suspense fallback={<Loader />}>
                 <main>
                     <Video />
                     <UsInfo />
@@ -24,7 +20,6 @@ function Inicio() {
                     <FindProduct />
                     <FarmacoVigilancia />
                 </main>
-            </Suspense>
         </>
     )
 }
