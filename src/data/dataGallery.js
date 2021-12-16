74d74adde6a63dde954d61fc5ae3c93be00e9714
Andres Martinez <andresmartgonz@gmail.com>
2021-12-16 12:07:43 +0100

    AboutUs gallery pics updated

--- src/data/dataGallery.js
+++ src/data/dataGallery.js
@@ -2,25 +2,25 @@ const dataGallery = [
   {
     id: 1,
     title: 'Planta',
-    imgPath: 'planta-produccion.png',
+    imgPath: 'planta-produccion.jpg',
     desc: 'Contamos con 75.000mts2 de capacidad instalada de producción, 6 plantas con  tecnología de punta en la industria farmacéutica global, para poder elaborar hasta 120 millones de unidades al año, en 70 líneas de producción abarcando más de 22 formas galénicas.',
   },
   {
     id: 2,
     title: 'Innovación',
-    imgPath: 'espacio.png',
+    imgPath: 'innovacion.jpg',
     desc: 'Desde nuestro origen siempre hemos descubierto nuevas razones para innovar, promoviendo constantemente el desarrollo de la salud, siendo pioneros en la creación de diversas fórmulas galénicas, genéricas y de nanotecnología en la región.'
   },
   {
     id: 3,
     title: 'Portafolio',
-    imgPath: 'eticos.jpg',
+    imgPath: 'portafolio.jpg',
     desc: 'Contamos con más de 600 registros en las principales líneas éticas, génericas, OTC y de fitofármacos, apalancados en una robusta cadena de distribución y soportados por un almacén robotizado con capacidad de 16 millones de unidades.'
   },
   {
     id: 4,
     title: 'Equipo',
-    imgPath: 'natural.jpg',
+    imgPath: 'equipo.jpg',
     desc: 'Nuestras operaciones solo son posibles gracias a lo mejor de nuestro talento humano, 850 colaboradores muy involucrados y especializados en producir eficaz y oportunamente, soluciones farmacéuticas al servicio de la vida humana.'
   },
   {
