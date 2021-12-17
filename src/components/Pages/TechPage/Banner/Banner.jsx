8cd00319d2e756a92ee97d7047e38c7e10c594a5
Andres Martinez <andresmartgonz@gmail.com>
2021-12-17 18:42:16 +0100

    rct054

--- src/components/Pages/TechPage/Banner/Banner.jsx
+++ src/components/Pages/TechPage/Banner/Banner.jsx
@@ -5,18 +5,18 @@ import {Fade} from 'react-awesome-reveal'
 function Banner() {
 
     return (
-        <section className="container-fluid Banner__IYD">
+        <section className="container-fluid Banner__Tech">
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-sm-7 Banner__IYD__title">
+                    <div className="col-12 col-sm-7 Banner__Tech__title">
                         <Fade delay={300} direction="left" triggerOnce>
-                            <h1>Pilares de I&D</h1>
-                            <h3>Para nosotros siempre ha sido prioridad contar con la tecnlogía e infraestructura que nos permita desarrollar los mejores productos, y además en las cantidades necesarias para cuidar de la salud de todo el país.</h3>
+                            <h1>Tecnología</h1>
+                            <h3>Nuestra planta está ubicada en Guarenas, estado Miranda, y es la planta producción de fármacos más grande a nivel nacional.<br /><br />Cuenta con la única planta de cefalosporínicos existentes en Venezuela y una de las pocas penicilínicos, siendo modelo de Latinoamérica.</h3>
                         </Fade>
                     </div>
                     <Fade cascade delay={300} triggerOnce>
-                        <div className="Banner__IYD__blue parallax" data-speed="-0.08" data-axis="vertical"></div>
-                        <div className="Banner__IYD__bg parallax" data-speed="-.1" data-axis="vertical"></div>
+                        <div className="Banner__Tech__blue parallax" data-speed="-0.08" data-axis="vertical"></div>
+                        <div className="Banner__Tech__bg parallax" data-speed="-.1" data-axis="vertical"></div>
                     </Fade>
                 </div>
             </div>
