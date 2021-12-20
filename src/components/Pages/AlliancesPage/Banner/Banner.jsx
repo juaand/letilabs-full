9eaf8e0b87269e18215e0c97ed19cac8ff0d4779
Andres Martinez <andresmartgonz@gmail.com>
2021-12-20 14:04:45 +0100

    rct067

--- src/components/Pages/AlliancesPage/Banner/Banner.jsx
+++ src/components/Pages/AlliancesPage/Banner/Banner.jsx
@@ -5,20 +5,19 @@ import {Fade} from 'react-awesome-reveal'
 function Banner() {
 
     return (
-        <section className="container-fluid Banner__Manufacture">
+        <section className="container-fluid Banner__Alliances">
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-sm-7 Banner__Manufacture__title">
+                    <div className="col-12 col-sm-7 Banner__Alliances__title">
                         <Fade delay={300} direction="left" triggerOnce>
-                            <h1>Manufactura</h1>
-                            <h3>Diariamente se manufacturan XX cantidades de todo tipo de medicinas, que salen de la planta para ser distribuidos en todo el país.<br /><br />
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eleifend accumsan mi, nec ultricies sapien imperdiet sed.Donec auctor, arcu nec lobortis rhoncus, lorem mi mattis massa.
+                            <h1>Alianzas</h1>
+                            <h3>Para lograr nuestro propósito de cuidar de la salud de los venezolanos, es importante contar con aliados que aporten al proceso y nos ayuden a ofrecer lo mejor.
                             </h3>
                         </Fade>
                     </div>
                     <Fade cascade delay={300} triggerOnce>
-                        <div className="Banner__Manufacture__blue parallax" data-speed="-0.08" data-axis="vertical"></div>
-                        <div className="Banner__Manufacture__bg parallax" data-speed="-.1" data-axis="vertical"></div>
+                        <div className="Banner__Alliances__blue parallax" data-speed="-0.08" data-axis="vertical"></div>
+                        <div className="Banner__Alliances__bg parallax" data-speed="-.1" data-axis="vertical"></div>
                     </Fade>
                 </div>
             </div>
