a7a1b94f52d563b542041ea70c82524bcc94e267
Andres Martinez <andresmartgonz@gmail.com>
2021-12-20 12:45:02 +0100

    rct059

--- src/components/Pages/ManufacturePage/Banner/Banner.jsx
+++ src/components/Pages/ManufacturePage/Banner/Banner.jsx
@@ -5,18 +5,20 @@ import {Fade} from 'react-awesome-reveal'
 function Banner() {
 
     return (
-        <section className="container-fluid Banner__Tech">
+        <section className="container-fluid Banner__Manufacture">
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-sm-7 Banner__Tech__title">
+                    <div className="col-12 col-sm-7 Banner__Manufacture__title">
                         <Fade delay={300} direction="left" triggerOnce>
-                            <h1>Tecnología</h1>
-                            <h3>Nuestra planta está ubicada en Guarenas, estado Miranda, y es la planta producción de fármacos más grande a nivel nacional.<br /><br />Cuenta con la única planta de cefalosporínicos existentes en Venezuela y una de las pocas penicilínicos, siendo modelo de Latinoamérica.</h3>
+                            <h1>Manufactura</h1>
+                            <h3>Diariamente se manufacturan XX cantidades de todo tipo de medicinas, que salen de la planta para ser distribuidos en todo el país.<br /><br />
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.Curabitur eleifend accumsan mi, nec ultricies sapien imperdiet sed.Donec auctor, arcu nec lobortis rhoncus, lorem mi mattis massa.
+                            </h3>
                         </Fade>
                     </div>
                     <Fade cascade delay={300} triggerOnce>
-                        <div className="Banner__Tech__blue parallax" data-speed="-0.08" data-axis="vertical"></div>
-                        <div className="Banner__Tech__bg parallax" data-speed="-.1" data-axis="vertical"></div>
+                        <div className="Banner__Manufacture__blue parallax" data-speed="-0.08" data-axis="vertical"></div>
+                        <div className="Banner__Manufacture__bg parallax" data-speed="-.1" data-axis="vertical"></div>
                     </Fade>
                 </div>
             </div>
