3fc2c3c1d8639a9b028578f19d7c59f85048fb03
Andres Martinez <andresmartgonz@gmail.com>
2021-12-21 17:25:47 +0100

    vadevecum model

--- src/components/Pages/SingleProductPage/SingleProductPage.jsx
+++ src/components/Pages/SingleProductPage/SingleProductPage.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import './SingleProductPage.css'
+import React, {useState, useEffect} from 'react'
 
 function SingleProductPage(props) {
 
     const buscar = props?.location?.state?.buscar
     const especialidad = props?.location?.state?.especialidad
 
+    useEffect(() => {
+        const fetchData = async () => {
+            const getBannerData = await getProduct()
+            setBannerData(getBannerData)
+        }
+        fetchData()
+    }, [buscar])
+
     return (
-        <div>
-            <h1>{buscar}</h1>
-            <h1>{especialidad}</h1>
-        </div>
+        <section className="container SingleProductPage">
+            <div className="row">
+                <div className="col-12 SingleProductPage__back">
+                    <p>Regresar al listado de productos</p>
+                </div>
+                <div className="col-12 col-sm-6 SingleProductPage__pic"></div>
+                <div className="col-12 col-sm-6 SingleProductPage__info">
+                    <h1>{buscar}</h1>
+                </div>
+            </div>
+        </section>
     )
 }
 
