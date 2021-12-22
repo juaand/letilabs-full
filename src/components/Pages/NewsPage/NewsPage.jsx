b78f1341bee62b01bd6962b0561fe4bd868f4d98
Andres Martinez <andresmartgonz@gmail.com>
2021-12-22 18:35:25 +0100

    rct088

--- src/components/Pages/NewsPage/NewsPage.jsx
+++ src/components/Pages/NewsPage/NewsPage.jsx
@@ -1,5 +1,8 @@
+import './NewsPage.css'
 import React, {useState, useEffect} from 'react'
+import {drawTime} from '../../../helpers/globals'
 import {getNews} from '../../../services/ApiClient'
+import {Link} from 'react-router-dom'
 
 function NewsPage() {
 
@@ -9,15 +12,34 @@ function NewsPage() {
         const fetchData = async () => {
             const getNewsData = await getNews()
             setNewsData(getNewsData)
+            console.log(getNewsData)
         }
         fetchData()
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return (
-        <div>
-            {newsData.map(el => <h1>{el.title}</h1>)}
-        </div>
+        <>
+            {newsData?.filter(el => el.outstanding === true).map(el =>
+                <Link to={{
+                    pathname: `/noticia`,
+                    state: {
+                        data: el
+                    }
+                }} className="container-fluid p-0 NewsPage__hero" style={{
+                    background: `url(${el.urlToPic}) no-repeat center center / cover`,
+                }}>
+                    <div className="NewsPage__container container">
+                        <span>{drawTime(el.publishDate)}</span>
+                        <h1>{el.title}</h1>
+                        <h3>{el.subTitle}</h3>
+                    </div>
+                </Link>
+            )}
+            <div className="container NewsPage__lastest">
+                <h1>Lo último</h1>
+            </div>
+        </>
     )
 }
 
