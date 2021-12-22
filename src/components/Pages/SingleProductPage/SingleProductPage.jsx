99a572fc685adea711798856f20891f2ddf9116f
Andres Martinez <andresmartgonz@gmail.com>
2021-12-22 11:16:19 +0100

    Single Product page update

--- src/components/Pages/SingleProductPage/SingleProductPage.jsx
+++ src/components/Pages/SingleProductPage/SingleProductPage.jsx
@@ -12,25 +12,10 @@ function SingleProductPage(props) {
     useEffect(() => {
         const fetchData = async () => {
             const getProductData = await getProduct(buscar)
-            setProduct(getProductData[0])
+            setProduct(getProductData)
         }
         fetchData()
     }, [buscar])
-
-    // active_principle: "Levodropropizina"
-    // category: "Tos"
-    // composition: "Levodropropizina 0,6%"
-    // health_register: "E.F. 30.815/15"
-    // id: "94"
-    // indication: "Tratamiento sintomático de la tos no productiva (tos seca)"
-    // line: "OTC"
-    // name: "Antux"
-    // posology: "De 10 a 20 kg: 2 ml a 3 ml de la solución cada 8 horas\r\nDe 21 a 30 kg: 3,5ml a 5 ml de la solución cada 8 horas\r\nDe 31 a 40 kg: 5ml a 7 ml de la solución cada 8 horas"
-    // presentation: "Antux 0,6% solución oral x 180ml"
-    // therapeutic_group: "Antitusígeno"
-    // trademarks: ""
-    // tv_spot: []
-    // _id: "61c1ff2b67766a6eb0387bef"
 
     return (
         <section className="container SingleProductPage">
@@ -41,30 +26,30 @@ function SingleProductPage(props) {
                 <div className="col-12 SingleProductPage__product">
                     <div className="row">
                         <div className="col-12 col-sm-6 SingleProductPage__pic"></div>
-                        <div className="col-12 col-sm-6 SingleProductPage__info">
-
+                        {product.length > 1 ? <h1>Hay más de un producto</h1> : 
+                            <div className="col-12 col-sm-6 SingleProductPage__info">
+                        {console.log(product)}
                             <>
-                                <h1>{product?.name}</h1>
-                                <h2>{product?.active_principle}</h2>
-                                {product.therapeutic_group.split('/').map((item) => 
-                                <span>{item}</span>
-                            )}
-                                <p><strong>Composición</strong> {product?.composition}</p>
-                                <p><strong>Indicación</strong> {product?.indication}
+                                <h1>{product[0]?.name}</h1>
+                                <h2>{product[0]?.active_principle}</h2>
+                                {product[0]?.therapeutic_group?.length > 1 ? product[0]?.therapeutic_group.map(el => <span>{el}</span>) : <span>{product[0]?.therapeutic_group}</span>}
+                                <p><strong>Composición</strong> {product[0]?.composition}</p>
+                                <p><strong>Indicación</strong> {product[0]?.indication}
                                 </p>
-                                <p><strong>Presentación</strong> {product?.presentation}</p>
-                                <p><strong>Resgitro sanitario</strong> {product?.health_register}</p>
+                                <p><strong>Presentación</strong> {product[0]?.presentation}</p>
+                                <p><strong>Resgitro sanitario</strong> {product[0]?.health_register}</p>
                             </>
                         </div>
+                        }
+                        
                     </div>
                 </div>
                 <div className="col-12 SingleProductPage__posology">
                     <p>
                         <h2>Posología</h2>
                         <ul>
-                            {product.posology.split('/').map((item) => 
-                                <li>{item}</li>
-                            )}
+                            <li dangerouslySetInnerHTML={{__html: product.posology}}>
+                            </li>
                         </ul>
                     </p>
                 </div>
