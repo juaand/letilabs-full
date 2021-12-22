28a6f16c9ed5c8d8ae43369d8cb2ed2d5b60cf29
Andres Martinez <andresmartgonz@gmail.com>
2021-12-22 18:59:10 +0100

    rct089

--- src/components/Pages/NewsPage/NewsPage.jsx
+++ src/components/Pages/NewsPage/NewsPage.jsx
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react'
 import {drawTime} from '../../../helpers/globals'
 import {getNews} from '../../../services/ApiClient'
 import {Link} from 'react-router-dom'
+import {Fade} from 'react-awesome-reveal'
 
 function NewsPage() {
 
@@ -20,24 +21,43 @@ function NewsPage() {
 
     return (
         <>
-            {newsData?.filter(el => el.outstanding === true).map(el =>
+            {newsData?.filter(el => el?.outstanding === true).map(el =>
                 <Link to={{
                     pathname: `/noticia`,
                     state: {
                         data: el
                     }
                 }} className="container-fluid p-0 NewsPage__hero" style={{
-                    background: `url(${el.urlToPic}) no-repeat center center / cover`,
+                    background: `url(${el?.urlToPic}) no-repeat center center / cover`,
                 }}>
                     <div className="NewsPage__container container">
-                        <span>{drawTime(el.publishDate)}</span>
-                        <h1>{el.title}</h1>
-                        <h3>{el.subTitle}</h3>
+                        <Fade triggerOnce direction="left" cascade>
+                            <span>{drawTime(el?.publishDate)}</span>
+                            <h1>{el?.title}</h1>
+                            <h3>{el?.subTitle}</h3>
+                        </Fade>
                     </div>
                 </Link>
             )}
             <div className="container NewsPage__lastest">
                 <h1>Lo último</h1>
+                <div className="row justify-content-between">
+                    <Fade cascade delay={300} direction="up" triggerOnce className="card NewsPage__card col-12 col-sm-3">
+                        {newsData?.filter(el => el?.outstanding !== true).slice(0, 3).map(el =>
+                            <>
+                                <img src={el?.urlToPic} className="card-img-top" alt={el?.title} />
+                                <div className="card-body">
+                                <span className="card-time">{drawTime(el?.publishDate)}</span>
+                                    <p className="card-title">{el?.title}</p>
+                                    <h5 className="card-subtitle">{el?.subTitle}</h5>
+                                </div>
+                                <div className="card-footer">
+                                    <Link className="leti-btn"></Link>
+                                </div>
+                            </>
+                        )}
+                    </Fade>
+                </div>
             </div>
         </>
     )
