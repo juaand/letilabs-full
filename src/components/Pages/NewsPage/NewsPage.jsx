4bfaffdaf01b88b962cfc0b740a6e15ccb54d4b9
Andres Martinez <andresmartgonz@gmail.com>
2021-12-27 10:57:33 +0100

    rct090

--- src/components/Pages/NewsPage/NewsPage.jsx
+++ src/components/Pages/NewsPage/NewsPage.jsx
@@ -13,7 +13,6 @@ function NewsPage() {
         const fetchData = async () => {
             const getNewsData = await getNews()
             setNewsData(getNewsData)
-            console.log(getNewsData)
         }
         fetchData()
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -64,8 +63,55 @@ function NewsPage() {
                     </Fade>
                 </div>
             </div>
-            <div className="container NewsPage__lastest">
-                <h1>Lo más leído</h1>
+            <div className="container-fluid p-0 NewsPage__most">
+                <div className="container">
+                    <h1>Lo más leído</h1>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-sm-6 NewsPage__most-hero">
+                        <Fade cascade delay={300} direction="up" triggerOnce className="card NewsPage__card col-12">
+                            {newsData?.filter(el => el?.outstanding !== true).slice(3, 4).map(el =>
+                                <>
+                                    <img src={el?.urlToPic} className="card-img-top" alt={el?.title} />
+                                    <div className="card-body">
+                                        <span className="card-time">{drawTime(el?.publishDate)}</span>
+                                        <p className="card-title">{el?.title}</p>
+                                        <h5 className="card-subtitle">{el?.subTitle}</h5>
+                                    </div>
+                                    <div className="card-footer">
+                                        <Link to={{
+                                            pathname: `/noticia`,
+                                            state: {
+                                                data: el
+                                            }
+                                        }} className="leti-btn"></Link>
+                                    </div>
+                                </>
+                            )}
+                        </Fade>
+                    </div>
+                    <div className="col-12 col-sm-4 offset-sm-1 NewsPage__most-resume">
+                        <Fade cascade direction="right" triggerOnce className="card NewsPage__card col-12">
+                            {newsData?.filter(el => el?.outstanding !== true).slice(4, 7).map(el =>
+                                <>
+                                    <div className="card-body">
+                                        <span className="card-time">{drawTime(el?.publishDate)}</span>
+                                        <p className="card-title">{el?.title}</p>
+                                        <h5 className="card-subtitle">{el?.subTitle}</h5>
+                                    </div>
+                                    <div className="card-footer">
+                                        <Link to={{
+                                            pathname: `/noticia`,
+                                            state: {
+                                                data: el
+                                            }
+                                        }} className="leti-btn"></Link>
+                                    </div>
+                                </>
+                            )}
+                        </Fade>
+                    </div>
+                </div>
             </div>
         </>
     )
