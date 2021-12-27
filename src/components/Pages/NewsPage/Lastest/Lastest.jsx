a2cd91c2099cb6c0ba7f3d82f016a71276ea97ee
Andres Martinez <andresmartgonz@gmail.com>
2021-12-27 11:16:34 +0100

    NewsPage lastest component

---
+++ src/components/Pages/NewsPage/Lastest/Lastest.jsx
@@ -0,0 +1,37 @@
+import './Lastest.css'
+import React from 'react'
+import {Fade} from 'react-awesome-reveal'
+import {Link} from 'react-router-dom'
+import {drawTime} from '../../../../helpers/globals'
+
+function Lastest({newsData}) {
+    return (
+        <div className="container NewsPage__lastest">
+            <h1>Lo último</h1>
+            <div className="row justify-content-between">
+                <Fade cascade delay={300} direction="up" triggerOnce className="card NewsPage__card col-12 col-sm-3">
+                    {newsData?.map(el =>
+                        <>
+                            <img src={el?.urlToPic} className="card-img-top" alt={el?.title} />
+                            <div className="card-body">
+                                <span className="card-time">{drawTime(el?.publishDate)}</span>
+                                <p className="card-title">{el?.title}</p>
+                                <h5 className="card-subtitle">{el?.subTitle}</h5>
+                            </div>
+                            <div className="card-footer">
+                                <Link to={{
+                                    pathname: `/noticia`,
+                                    state: {
+                                        data: el
+                                    }
+                                }} className="leti-btn"></Link>
+                            </div>
+                        </>
+                    )}
+                </Fade>
+            </div>
+        </div>
+    )
+}
+
+export default Lastest
--- src/components/Pages/NewsPage/NewsPage.jsx
+++ src/components/Pages/NewsPage/NewsPage.jsx
@@ -5,6 +5,7 @@ import {getNews} from '../../../services/ApiClient'
 import {Link} from 'react-router-dom'
 import {Fade} from 'react-awesome-reveal'
 import Banner from './Banner/Banner'
+import Lastest from './Lastest/Lastest'
 
 function NewsPage() {
 
@@ -22,31 +23,7 @@ function NewsPage() {
     return (
         <main>
             <Banner newsData={newsData?.filter(el => el?.outstanding === true)}/>
-            <div className="container NewsPage__lastest">
-                <h1>Lo último</h1>
-                <div className="row justify-content-between">
-                    <Fade cascade delay={300} direction="up" triggerOnce className="card NewsPage__card col-12 col-sm-3">
-                        {newsData?.filter(el => el?.outstanding !== true).slice(0, 3).map(el =>
-                            <>
-                                <img src={el?.urlToPic} className="card-img-top" alt={el?.title} />
-                                <div className="card-body">
-                                    <span className="card-time">{drawTime(el?.publishDate)}</span>
-                                    <p className="card-title">{el?.title}</p>
-                                    <h5 className="card-subtitle">{el?.subTitle}</h5>
-                                </div>
-                                <div className="card-footer">
-                                    <Link to={{
-                                        pathname: `/noticia`,
-                                        state: {
-                                            data: el
-                                        }
-                                    }} className="leti-btn"></Link>
-                                </div>
-                            </>
-                        )}
-                    </Fade>
-                </div>
-            </div>
+            <Lastest newsData={newsData?.filter(el => el?.outstanding !== true).slice(0, 3)}/>
             <div className="container-fluid p-0 NewsPage__most">
                 <div className="container">
                     <h1>Lo más leído</h1>
