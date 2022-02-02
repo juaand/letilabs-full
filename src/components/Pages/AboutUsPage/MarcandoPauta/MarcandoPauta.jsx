3bce7b35c2a940321077e3110a10d018ce3b916c
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-02 15:31:58 +0100

    marcando pauta dinamic

--- src/components/Pages/AboutUsPage/MarcandoPauta/MarcandoPauta.jsx
+++ src/components/Pages/AboutUsPage/MarcandoPauta/MarcandoPauta.jsx
@@ -1,8 +1,21 @@
 import './MarcandoPauta.css'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {getMarcandoPauta} from '../../../../services/ApiClient'
 import {Fade} from "react-awesome-reveal"
 
 function MarcandoPauta() {
+
+    const [marcandoPautaData, setMarcandoPautaData] = useState()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const getMarcandoPautaData = await getMarcandoPauta()
+            setMarcandoPautaData(getMarcandoPautaData)
+        }
+        fetchData()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return (
         <section className="container-fluid MarcandoPauta">
             <div className="row justify-content-between">
@@ -10,14 +23,14 @@ function MarcandoPauta() {
                     <div className="row justify-content-center">
                         <div className="col-11 col-sm-9 offset-sm-5">
                             <Fade direction="down" triggerOnce>
-                                <p>
-                                    Contamos con el mejor talento que con su <span className="blue-text">ingenio</span> e <span className="blue-text">increíble calidad humana,</span> trabajan muy comprometidos día a día por los venezolanos, velando por la salud de todo un país.
-                                </p>
+                                <p dangerouslySetInnerHTML={{__html: marcandoPautaData?.description}} />
                             </Fade>
                         </div>
                     </div>
                 </div>
-                <div className="col-12 col-sm-6 MarcandoPauta__bg"></div>
+                <div className="col-12 col-sm-6 MarcandoPauta__bg" style={{
+                    background: `url(${marcandoPautaData?.imgURL}) no-repeat right bottom / contain`,
+                }} />
             </div>
         </section>
     )
