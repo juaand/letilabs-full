931fa7673b437fc145e347e4bc75463a92dcef3a
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-02 17:27:46 +0100

    banner dinamic

--- src/components/Pages/OurCompaniesPage/Banner/Banner.jsx
+++ src/components/Pages/OurCompaniesPage/Banner/Banner.jsx
@@ -1,20 +1,34 @@
 import './Banner.css'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Fade} from "react-awesome-reveal"
+import {getBannerOC} from '../../../../services/ApiClient'
 
 function Banner() {
+
+    const [bannerData, setBannerData] = useState()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const getBannerData = await getBannerOC()
+            setBannerData(getBannerData)
+        }
+        fetchData()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+
     return (
         <section className="container-fluid Banner__ourCompanies">
             <div className="container">
                 <div className="row d-flex">
                     <div className="col-12 col-sm-6 Banner__ourCompanies__title">
                         <Fade direction="left" duration={600} triggerOnce>
-                            <h1>Sustentados por tres grandes unidades de negocio, que se ocupan de áreas específicas, y se integran entre ellas para lograr <span className="blue-text">mejores resultados</span></h1>
+                            <h1 dangerouslySetInnerHTML={{__html: bannerData?.description}} />
                         </Fade>
                     </div>
                     <div className="col-12 col-sm-6 Banner__ourCompanies__bg">
-                        <Fade delay={300} duration={600} triggerOnce>
-                            <img src="/images/our-companies-bg.png" alt="Sobre nosotros banner" />
+                        <Fade delay={1000} duration={600} triggerOnce>
+                            <img src={bannerData?.imgURL} alt="Sobre nosotros banner" />
                         </Fade>
                     </div>
                 </div>
