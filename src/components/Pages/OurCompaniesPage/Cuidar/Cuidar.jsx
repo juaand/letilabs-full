f8b0a83cef06f71eed57d02b0531411030495b7f
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-02 17:55:51 +0100

    dinamic care

--- src/components/Pages/OurCompaniesPage/Cuidar/Cuidar.jsx
+++ src/components/Pages/OurCompaniesPage/Cuidar/Cuidar.jsx
@@ -1,19 +1,35 @@
 import './Cuidar.css'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Fade} from "react-awesome-reveal"
+import {getCareOC} from '../../../../services/ApiClient'
 
 function Cuidar() {
+
+    const [bannerData, setBannerData] = useState()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const getBannerData = await getCareOC()
+            setBannerData(getBannerData)
+        }
+        fetchData()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+
     return (
         <section className="container-fluid Cuidar">
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-12 Cuidar__clip">
                         <div className="Cuidar__stroke parallax" data-speed="-0.04" data-axis="horizontal" />
-                        <div className="Cuidar__clip-content parallax" data-speed="0.06" data-axis="vertical" />
+                        <div className="Cuidar__clip-content parallax" data-speed="0.06" data-axis="vertical" style={{
+                            background: `url(${bannerData?.imgURL}) no-repeat center center / cover`
+                        }} />
                     </div>
                     <div className="col-12 col-sm-5">
                         <Fade direction="down" triggerOnce>
-                            <h1 className="text-center">Cuidar la salud de los venezolanos</h1>
+                            <h1 className="text-center">{bannerData?.description}</h1>
                         </Fade>
                     </div>
                 </div>
