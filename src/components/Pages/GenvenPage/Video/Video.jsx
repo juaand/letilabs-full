4ab3b2b8c79e924a8091b7866d35064938df2470
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-03 11:48:59 +0100

    banner

--- src/components/Pages/GenvenPage/Video/Video.jsx
+++ src/components/Pages/GenvenPage/Video/Video.jsx
@@ -1,12 +1,33 @@
 import './Video.css'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {getApiVideo} from '../../../../services/ApiClient'
+import {getOurCompaniesVideoGenven} from '../../../../services/ApiClient'
 
 function Video() {
+
+    const [videoData, setVideoData] = useState()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const getVideoData = await getApiVideo()
+            setVideoData(getVideoData[0]?.url)
+        }
+        fetchData()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+
     return (
-        <section className="container-fluid Video__genven">
-            <img src="./images/play.svg" alt="play" />
+        <section className="container-fluid Video">
+            {videoData && (
+                <video loop muted autoPlay playsInline>
+                    <source src={videoData} type="video/mp4" />
+                </video>
+            )}
         </section>
     )
 }
+
+
 
 export default Video
