18af5d1d1dd10563c393a3406e910cba272119c2
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-03 10:29:40 +0100

    banner dinamic

--- src/components/Pages/LaboratoriosLetiPage/Banner/Banner.jsx
+++ src/components/Pages/LaboratoriosLetiPage/Banner/Banner.jsx
@@ -1,24 +1,38 @@
 import './Banner.css'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Fade} from 'react-awesome-reveal'
+import {getBannerOCLeti} from '../../../../services/ApiClient'
 
 function Banner() {
+
+    const [bannerData, setBannerData] = useState()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const getBannerData = await getBannerOCLeti()
+            setBannerData(getBannerData)
+        }
+        fetchData()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     return (
         <section className="container-fluid Banner__Leti">
             <div className="container">
                 <div className="row">
                     <div className="col-12 col-sm-7 Banner__Leti__title">
-                        <Fade triggerOnce>
-                            <img className="Banner__Leti-logo" src="./images/leti.svg" alt="Leti" />
+                        <Fade triggerOnce delay={1000}>
+                            <img className="Banner__Leti-logo" src={bannerData?.logoURL} alt="Leti" />
                         </Fade>
-                        <Fade delay={300} direction="left" triggerOnce>
-                            <h3>Esta es la unidad que se encarga de desarrollar la gama de productos que abarca diferentes áreas terapéuticas: cardiovascular, metabolismo, gástrica, respiratoria, neurológicas, músculo-esqueléticas, dolor, antibióticos, vitaminas, tanto para el paciente pediátrico como para el paciente adulto.</h3>
+                        <Fade delay={1300} direction="left" triggerOnce>
+                            <h3>{bannerData?.description}</h3>
                         </Fade>
                     </div>
                     <Fade cascade delay={300} triggerOnce>
                         <div className="Banner__Leti__blue parallax" data-speed="-0.08" data-axis="vertical"></div>
-                        <div className="Banner__Leti__bg parallax" data-speed="-.1" data-axis="vertical"></div>
+                        <div className="Banner__Leti__bg parallax" data-speed="-.1" data-axis="vertical" style={{
+                            background: `url(${bannerData?.imgURL}) no-repeat center / contain`,
+                        }}></div>
                     </Fade>
                 </div>
             </div>
