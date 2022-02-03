36546d3a22eac8c3dd3c4b6ea8357cf76e25546a
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-03 19:32:20 +0100

    bottomCta our companies page bg updated

--- src/components/Pages/OurCompaniesPage/BottomCta/BottomCta.jsx
+++ src/components/Pages/OurCompaniesPage/BottomCta/BottomCta.jsx
@@ -24,8 +24,10 @@ function BottomCta() {
             <div className="row">
                 {ourCompaniesOCData?.map((el, key) =>
                     <div className="col-12 col-sm-6 p-0 BottomCta__img">
-                        {key === 0 ? <div className="BottomCta__talento" /> :
-                            <div className="BottomCta__megat" />}
+                        {key === 0 ? <div className="BottomCta__talento" style={{
+                            background: `url("${el?.img}") no-repeat center center / cover`}}/> :
+                            <div className="BottomCta__megat" style={{
+                            background: `url("${el?.img}") no-repeat center center / cover`}}/>}
                         <div className="BottomCta__title">
                             <Fade {...key > 1 && `delay=${300}`} direction="up" triggerOnce>
                                 <h2>{el?.title}</h2>
