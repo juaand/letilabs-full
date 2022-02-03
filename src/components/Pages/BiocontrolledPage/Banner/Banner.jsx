d3d1c7e23f82622804fcc0326677df88c6803950
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-03 12:36:33 +0100

    banner

--- src/components/Pages/BiocontrolledPage/Banner/Banner.jsx
+++ src/components/Pages/BiocontrolledPage/Banner/Banner.jsx
@@ -1,24 +1,38 @@
 import './Banner.css'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Fade} from 'react-awesome-reveal'
+import {getBannerOCBiocontrolled} from '../../../../services/ApiClient'
 
 function Banner() {
+
+    const [bannerData, setBannerData] = useState()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const getBannerData = await getBannerOCBiocontrolled()
+            setBannerData(getBannerData)
+        }
+        fetchData()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     return (
         <section className="container-fluid Banner__Biocontrolled">
             <div className="container">
                 <div className="row">
                     <div className="col-12 col-sm-7 Banner__Biocontrolled__title">
-                        <Fade triggerOnce>
-                            <img className="Banner__Biocontrolled-logo" src="./images/biocontrolled.svg" alt="Biocontrolled logo" />
+                        <Fade delay={1000} triggerOnce>
+                            <img className="Banner__Biocontrolled-logo" src={bannerData?.logoURL} alt="Biocontrolled logo" />
                         </Fade>
-                        <Fade delay={300} direction="left" triggerOnce>
-                            <h3>Esta es la unidad de explorar nuevas maneras y eficaces maneras de desarrollar medicamentos, gracias a <span className="blue-text">Biocontrolled</span> es que nos mantenemos a la vanguardia y podemos seguir ofreciendo productos cada vez más beneficiosos.</h3>
+                        <Fade delay={1300} direction="left" triggerOnce>
+                            <h3 dangerouslySetInnerHTML={{__html: bannerData?.description}} />
                         </Fade>
                     </div>
                     <Fade cascade delay={300} triggerOnce>
                         <div className="Banner__Biocontrolled__blue parallax" data-speed="-0.08" data-axis="vertical"></div>
-                        <div className="Banner__Biocontrolled__bg parallax" data-speed="-.1" data-axis="vertical"></div>
+                        <div className="Banner__Biocontrolled__bg parallax" data-speed="-.1" data-axis="vertical" style={{
+                            background: `url(${bannerData?.imgURL}) no-repeat center / contain`,
+                        }}></div>
                     </Fade>
                 </div>
             </div>
