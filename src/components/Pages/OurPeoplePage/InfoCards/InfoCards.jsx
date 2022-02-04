0c3cc01f634de5ee3a1bc4dbd5173e1bde3bd321
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-04 17:15:27 +0100

    infocards

--- src/components/Pages/OurPeoplePage/InfoCards/InfoCards.jsx
+++ src/components/Pages/OurPeoplePage/InfoCards/InfoCards.jsx
@@ -1,14 +1,28 @@
 import './InfoCards.css'
-import React from 'react'
-import dataOurPeople from '../../../../data/dataOurPeople'
+import React, {useState, useEffect} from 'react'
 import {Fade} from 'react-awesome-reveal'
+import {getInfoCardsOurPeople} from '../../../../services/ApiClient'
 
 
 function InfoCards() {
+
+    const [dataOurPeople, setDataOurPeople] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const getInfoCardsData = await getInfoCardsOurPeople()
+            setDataOurPeople(getInfoCardsData)
+        }
+        fetchData()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return (
-        <section className="container-fluid InfoCards__OurPeople">
+        <section className="container-fluid InfoCards__OurPeople" style={{
+            background: `url(${dataOurPeople[0]?.imgURL}) no-repeat top center / cover`,
+        }}>
             <Fade triggerOnce>
-                <h1>Somos tres equipos trabajando en constante sinergia</h1>
+                <h1>{dataOurPeople[0]?.mainTitle}</h1>
             </Fade>
             <div className="container-fluid InfoCards">
                 <div className="row justify-content-center">
