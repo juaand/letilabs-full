3301b7af8186a82ea0700ef55145ad658a7916cb
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-09 16:15:40 +0100

    EditBottomAlliances cleaned

--- src/components/Pages/AdminEditPage/EditIAndDAlliances/EditBottomAlliances/EditBottomAlliances.jsx
+++ src/components/Pages/AdminEditPage/EditIAndDAlliances/EditBottomAlliances/EditBottomAlliances.jsx
@@ -1,39 +1,10 @@
 import React, {useState, useEffect} from 'react'
-import {useFormState} from '../../../../../hooks/useFormState'
-import {getBottomAlliancesData, updateBottomAlliances} from '../../../../../services/ApiClient'
-import InputWithLabel from '../../../../Form/InputWithLabel/InputWithLabel'
-import Button from '../../../../Form/FormButton/FormButton'
+import {getBottomAlliancesData} from '../../../../../services/ApiClient'
 import DeleteItemModal from './DeleteItemModal/DeleteItemModal'
 
 
 
 function EditBottomAlliances() {
-    const {state, onBlur, onChange} = useFormState(
-        {
-            data: {
-                title: '',
-                img: '',
-                url: '',
-                button: '',
-            },
-            error: {
-                title: false,
-                img: false,
-                url: false,
-                button: false,
-            },
-            touch: {},
-        },
-        {
-            title: v => v.length,
-            img: v => v.length,
-            url: v => v.length,
-            button: v => v.length,
-        }
-    )
-
-    const {data, error, touch} = state
-    const [registerError, setRegisterError] = useState(null)
     const [modalData, setModalData] = useState()
     const [IDData, setIDData] = useState()
     const [bool, setBool] = useState(false)
@@ -67,7 +38,7 @@ function EditBottomAlliances() {
                     <div className="row justify-content-around">
                         {IDData?.map(el =>
                             <div className="col-1 EditCarousel__trash" onClick={() => showModal(el)}>
-                                <img className="EditCarousel__img" src={el.img.toLowerCase()} alt={el.img} />
+                                <img className="EditCarousel__img" src={el.img} alt={el.img} />
                                 <p>{el.title}</p>
                                 <p>{el.button}</p>
                                 <p>{el.url}</p>
