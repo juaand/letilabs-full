27012f7b0ec6f1678254fd7f8c0078782abdba26
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-14 16:35:48 +0100

    update product list

---
+++ src/components/Pages/AdminEditPage/EditProductsList/EditProductsList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import EditProductListBanner from './EditProductListBanner/EditProductListBanner'
+
+function EditProductsList() {
+    return (
+
+        <main>
+            <h2 className="EditContent EditContent__title">Editar página listado de productos</h2>
+            <EditProductListBanner />
+        </main>
+    )
+}
+
+export default EditProductsList
