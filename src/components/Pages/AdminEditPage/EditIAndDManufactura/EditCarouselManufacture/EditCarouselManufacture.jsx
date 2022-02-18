40250a701e5335639fd16f6064223f6617e95f7d
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-18 11:26:08 +0100

    CRUD manufacture title proccess update

--- src/components/Pages/AdminEditPage/EditIAndDManufactura/EditCarouselManufacture/EditCarouselManufacture.jsx
+++ src/components/Pages/AdminEditPage/EditIAndDManufactura/EditCarouselManufacture/EditCarouselManufacture.jsx
@@ -1,12 +1,38 @@
 import React, {useState, useEffect} from 'react'
-import {getCarouselManufacture} from '../../../../../services/ApiClient'
-import DeleteItemModal from './DeleteItemModal/DeleteItemModal'
+
+import InputWithLabel from '../../../../Form/InputWithLabel/InputWithLabel'
+import {getCarouselManufacture, updateTitleProccess} from '../../../../../services/ApiClient'
+import {useFormState} from '../../../../../hooks/useFormState'
+import Button from '../../../../Form/FormButton/FormButton'
+import EditItemModal from './EditItemModal/EditItemModal'
 
 function EditCarouselManufacture() {
-    
-    const [modalData, setModalData] = useState()
-    const [ourCarouselManufactureData, setOurCarouselManufactureData] = useState()
+
+    const [carouselManufactureData, setCarouselManufactureData] = useState([])
+    const [modalData, setModalData] = useState([])
+    const [message, setMessage] = useState('')
     const [bool, setBool] = useState(false)
+
+    const {state, onChange} = useFormState(
+        {
+            data: {
+                title: '',
+                picPath: '',
+            },
+            error: {
+                title: true,
+                picPath: true,
+            },
+            touch: {},
+        },
+        {
+            title: v => v.length,
+            picPath: v => v.length,
+        }
+    )
+
+    const {data, error} = state
+    const [registerError, setRegisterError] = useState(null)
 
     const showModal = (data) => {
         setModalData(data)
@@ -14,14 +40,35 @@ function EditCarouselManufacture() {
     }
 
     const deleteItem = (data) => {
-        setOurCarouselManufactureData(data)
+        setCarouselManufactureData(data)
         setBool(!bool)
+    }
+
+    const updateInfo = async (event) => {
+        event.preventDefault()
+
+        if (error.title === false) {
+            try {
+                await updateTitleProccess(data)
+                    .then(info => {
+                        setCarouselManufactureData(info)
+                        setMessage('Título atualizado exitosamente')
+                    })
+                    .catch(error => {
+                        setRegisterError(error)
+                    })
+            } catch (err) {
+                setRegisterError(err.response?.data?.message)
+            }
+        } else {
+            setMessage('Por favor edite el campo')
+        }
     }
 
     useEffect(() => {
         const fetchData = async () => {
             const getOurCarouselManufactureData = await getCarouselManufacture()
-            setOurCarouselManufactureData(getOurCarouselManufactureData)
+            setCarouselManufactureData(getOurCarouselManufactureData)
 
         }
         fetchData()
@@ -30,18 +77,45 @@ function EditCarouselManufacture() {
 
     return (
         <>
-            {bool && <DeleteItemModal hideModal={() => setBool(!bool)} data={modalData} deleteItem={(updateData) => deleteItem(updateData)} />}
-            {ourCarouselManufactureData?.length > 0 &&
-                <section className="container-fluid  EditContent EditContent-timeline">
-                    <h2>Editar procesos</h2>
+            {bool && <EditItemModal hideModal={() => setBool(!bool)} infodata={modalData} deleteItem={(updateData) => deleteItem(updateData)} closeModal={() => setBool(!bool)} />}
+            {carouselManufactureData?.length > 0 &&
+
+                <section className="container-fluid  EditContent EditContent-timeline pt-0">
+                    <h2>Procesos</h2>
                     <div className="row justify-content-around">
-                        {ourCarouselManufactureData?.map(el =>
+                        <form className="AdminEdit__form" onSubmit={updateInfo}>
+                            <div className="row">
+                                <h3 className="mt-5">Editar título procesos</h3>
+                                <div className="col-12">
+                                    <InputWithLabel
+                                        value={data.title}
+                                        label="Título carrusel"
+                                        onChange={onChange}
+                                        name="title"
+                                        type="text"
+                                        cssStyle="form-control mb-5"
+                                        placeholder={carouselManufactureData[0]?.title}
+                                    />
+                                </div>
+                                <div className="col-12 col-sm-6">
+                                    <Button type="submit" cssStyle="leti-btn">Editar título</Button>
+                                    {message && <span className="AdminEdit__message ">{message}</span>}
+                                </div>
+                            </div>
+
+                            <hr className="mt-5 mb-5"/>
+                            
+                            {registerError && <div className="alert alert-danger">{registerError}</div>}
+                        </form>
+                        <h3 className="mb-5">Editar Procesos</h3>
+                        {carouselManufactureData?.map(el =>
                             <div className="col-3 EditCarousel__edit logros" onClick={() => showModal(el)}>
                                 <p>{el?.info}</p>
                             </div>
                         )}
                     </div>
-                </section>}
+                </section>
+            }
         </>
     )
 }
