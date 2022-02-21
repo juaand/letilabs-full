ee31fd1881420544298ded93078bee66606ea609
Andrés Martínez González <andresmartgonz@gmail.com>
2022-02-21 13:37:56 +0100

    CRUD tech carousel delete item

--- src/components/Pages/AdminEditPage/EditIAndDTechnology/EditCarrouselTech/EditCarrouselTech.jsx
+++ src/components/Pages/AdminEditPage/EditIAndDTechnology/EditCarrouselTech/EditCarrouselTech.jsx
@@ -1,10 +1,10 @@
 import React, {useState, useEffect} from 'react'
 import {getCarouselTech} from '../../../../../services/ApiClient'
-import DeleteItemModal from './DeleteItemModal/DeleteItemModal'
+import EditItemModal from './EditItemModal/EditItemModal'
 
 function EditCarrouselTech() {
-    const [modalData, setModalData] = useState()
-    const [ourGoalsOCData, setOurGoalsOCData] = useState()
+    const [modalData, setModalData] = useState([])
+    const [ourGoalsOCData, setOurGoalsOCData] = useState([])
     const [bool, setBool] = useState(false)
 
     const showModal = (data) => {
@@ -17,11 +17,16 @@ function EditCarrouselTech() {
         setBool(!bool)
     }
 
+    const hideModal = (data) => {
+        console.log(data)
+        // ourGoalsOCData(data)
+        setBool(!bool)
+    }
+
     useEffect(() => {
         const fetchData = async () => {
             const getOurGoalsOCData = await getCarouselTech()
             setOurGoalsOCData(getOurGoalsOCData)
-
         }
         fetchData()
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -29,13 +34,13 @@ function EditCarrouselTech() {
 
     return (
         <>
-            {bool && <DeleteItemModal hideModal={() => setBool(!bool)} data={modalData} deleteItem={(updateData) => deleteItem(updateData)} />}
+            {bool && <EditItemModal hideModal={(data) => hideModal(data)} infodata={modalData} deleteItem={(updateData) => deleteItem(updateData)} closeModal={() => setBool(!bool)}/>}
             {ourGoalsOCData?.length > 0 &&
                 <section className="container-fluid EditContent EditContent-timeline">
                     <h2>Editar Carousel</h2>
                     <div className="row justify-content-around">
                         {ourGoalsOCData?.map(el =>
-                            <div className="col-3 EditCarousel__edit logros" onClick={() => showModal(el)}>
+                            <div className="col EditCarousel__edit logros" onClick={() => showModal(el)}>
                                 <img src={el?.imgURL} alt={el?.name}  />
                                 <h4 className="mt-5">{el?.title}</h4>
                             </div>
---
+++ src/components/Pages/AdminEditPage/EditIAndDTechnology/EditCarrouselTech/EditItemModal/EditItemModal.jsx
@@ -0,0 +1,194 @@
+import React, {useState} from 'react'
+import {Editor} from '@tinymce/tinymce-react'
+import {Fade} from 'react-awesome-reveal'
+
+import './EditItemModal.css'
+import {deleteCarouselTech, updateCarouselTech} from '../../../../../../services/ApiClient'
+import InputWithLabel from '../../../../../Form/InputWithLabel/InputWithLabel'
+import {useFormState} from '../../../../../../hooks/useFormState'
+import InputFile from '../../../../../Form/InputFile/InputFile'
+import Button from '../../../../../Form/FormButton/FormButton'
+import {app} from '../../../../../../services/firebase'
+import Loader from '../../../../../Loader/Loader'
+
+function EditItemModal({deleteItem, infodata, hideModal, closeModal}) {
+
+    const [timelineData, setTimelineData] = useState(infodata)
+    const [imageSuccess, setImageSuccess] = useState('')
+    const [isDisabled, setIsDisabled] = useState(false)
+    const [message, setMessage] = useState('')
+
+    const {state, onChange} = useFormState(
+        {
+            data: {
+                id: timelineData?._id,
+                title: timelineData?.title,
+                imgURL: timelineData?.imgURL,
+                description: timelineData?.description,
+            },
+            error: {
+                imgURL: true,
+                description: true,
+                title: true,
+            },
+            touch: {},
+        },
+        {
+            imgURL: v => v.length,
+            description: v => v.length,
+            title: v => v.length,
+        }
+    )
+
+    const {data, error} = state
+    const [registerError, setRegisterError] = useState(null)
+
+    const onFileSelected = async (e) => {
+        setMessage('')
+        setIsDisabled(!isDisabled)
+
+        // Get file
+        const file = e.target.files[0]
+
+        // Create storage ref
+        const storageRef = app.storage().ref()
+        const filePath = storageRef.child('images/' + file.name)
+
+        // Upload file
+        await filePath.put(file)
+            .then(() => {
+                setImageSuccess("Imagen subida correctamente")
+            })
+            .catch(err => {console.log(err)})
+
+        // Get file url
+        const fileUrl = await filePath.getDownloadURL()
+        data.imgURL = fileUrl
+        setIsDisabled(false)
+        error.imgURL = false
+    }
+
+    const updateInfo = async (event) => {
+        event.preventDefault()
+
+        if (Object.values(error).map(el => el).includes(false)) {
+            try {
+                await updateCarouselTech(data)
+                    .then(info => {
+                        setTimelineData(info)
+                        setMessage('Data atualizada exitosamente')
+                        hideModal(info)
+                    })
+                    .catch(error => {
+                        setRegisterError(error)
+                    })
+            } catch (err) {
+                setRegisterError(err.response?.data?.message)
+            }
+        } else {
+            setMessage('Por favor complete alguno de los campos')
+        }
+    }
+
+    const handleDescription = (e) => {
+        data.description = e.target.getContent()
+        error.description = false
+    }
+
+    const deleteSelected = async (id) => {
+        const updatedData = await deleteCarouselTech(id)
+        deleteItem(updatedData)
+    }
+
+
+    return (
+        <>
+            {isDisabled && <Loader message="Cargando imagen..." />}
+            <div className="EditItemModal">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <Fade direction="down" className="col-11 col-sm-6 EditItemModal__container">
+                            <>
+                                <span className="EditItemModal__close" onClick={closeModal}></span>
+                                <form className="AdminEdit__form" onSubmit={updateInfo}>
+                                    <div className="row">
+                                        <div className="col-sm-12 ShowEditModal__thumbnail mb-5">
+                                            <div className="ShowEditModal__thumbnail-img" style={{
+                                                background: `url(${data?.imgURL}) no-repeat center center / cover`,
+                                            }}></div>
+                                            <h1 className="DeleteItemModal__ask">Editar elemento 
+                                            <span class="ShowEditModal__news-title">{timelineData?.title}</span></h1>
+                                        </div>
+                                        <div className="col-12">
+                                            <p className="AdminEdit__form__label">
+                                                Título
+                                            </p>
+                                            <InputWithLabel
+                                                value={data?.title}
+                                                onChange={onChange}
+                                                name="title"
+                                                type="text"
+                                                cssStyle="form-control"
+                                                placeholder={timelineData?.title}
+                                            />
+                                        </div>
+                                        <div className="col-12">
+                                            <p className="AdminEdit__form__label">
+                                                Editar imagen
+                                            </p>
+                                            <InputFile
+                                                value={data?.imgURL}
+                                                onChange={onFileSelected}
+                                                id="fileButton"
+                                                name="imgURL"
+                                                type="file"
+                                                placeholder={timelineData?.imgURL}
+                                                classStyle="mb-0"
+                                            />
+                                            {imageSuccess && <span className="AdminEdit__message mt-1">{imageSuccess}</span>}
+                                        </div>
+                                        <div className="col-12">
+                                            <p className="AdminEdit__form__label mt-5">
+                                                Editar descripción
+                                            </p>
+                                            <Editor
+                                                initialValue={timelineData?.description}
+                                                onChange={handleDescription}
+                                                apiKey={process.env.REACT_APP_API_TINY_CLOUD}
+                                                init={{
+                                                    height: 200,
+                                                    menubar: false,
+                                                    plugins: [
+                                                        'advlist autolink lists link image',
+                                                        'charmap print preview anchor help',
+                                                        'searchreplace visualblocks code',
+                                                        'insertdatetime media table paste wordcount'
+                                                    ],
+                                                    toolbar:
+                                                        'bold',
+                                                }}
+                                            />
+                                        </div>
+                                        <div className="col-12 col-sm-6 mt-5">
+                                            <div onClick={() => deleteSelected(timelineData?._id)} className="leti-btn delete">Eliminar elemento</div>
+                                        </div>
+                                        <div className="col-12 col-sm-6 mt-5 d-flex justify-content-end">
+                                            <Button type="submit" cssStyle="leti-btn">Guardar cambios</Button>
+                                        </div>
+                                        {message &&
+                                            <div className="row">
+                                                <span className="AdminEdit__message col-12 d-flex justify-content-end">{message}</span>
+                                            </div>}
+                                    </div>
+                                    {registerError && <div className="alert alert-danger">{registerError}</div>}
+                                </form>
+                            </>
+                        </Fade>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default EditItemModal
